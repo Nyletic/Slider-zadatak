9ab8700b55b424145d80ea7b3e1b0f0ad64baa19
Mihael Nikolić <39503541+Nyletic@users.noreply.github.com>
2023-12-09 03:49:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 jQuery(function ($) {
-  const animationDuration = 400;
+  const animationDuration = 300;
 
   $("#prev").on("click", function () {
     $(".carousels .images img:first-child").fadeOut(
       animationDuration,
       function () {
-        $(this)
-          .appendTo($(this).parent())
-          .fadeIn(animationDuration)
-          .animate(animationDuration);
+        $(this).appendTo($(this).parent()).fadeIn(animationDuration);
       }
     );
   });
@@ -22,12 +19,3 @@ jQuery(function ($) {
     );
   });
 });
-
-// $("#next").on("click", function () {
-//   $(".carousel .images img:last-child").hide("normal", function () {
-//     $(this)
-//       .prependTo($(this).parent())
-//       .show("normal")
-//       .css({ marginLeft: "5px" });
-//   });
-// });
